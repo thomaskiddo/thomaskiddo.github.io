eb7f067182a11ad73e2f0e5a86954a02f6ab49d1
thomaskiddo <30905091+thomaskiddo@users.noreply.github.com>
2017-12-04 09:15:24 -0800

    Add files via upload

---
+++ Thomas.py
@@ -0,0 +1,20 @@
+x=int(input("Please enter a number and I will double it: "))
+print(x*2)
+
+num = int(input("Enter a number and I will determine if it is odd or even: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+import turtle
+sven=turtle.Turtle()
+y=int(input("Enter the number 90 and I'll make a square: "))
+for i in range(4):
+    sven.forward(y)
+    sven.right(y)
+
+
+
+
